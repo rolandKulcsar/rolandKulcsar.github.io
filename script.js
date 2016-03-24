2ffcce77b03d4c002e3f4639e06dad7af1966289
Kulcsár Roland <roolandeu@gmail.com>
2016-03-24 19:24:29 +0100

    Added distance metrics

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
 ﻿var canvas = document.getElementById("canvas_voronoi");
 var context = canvas.getContext("2d");
+var metricSelector = document.getElementById("select_distance");
 
 var controlPoints = new Array();
 
 var epsilon = 0.01;
-var controlPointRadius = 15;
+var controlPointRadius = 10;
 var size = canvas.width;
 
 canvas.addEventListener("mousedown", function (evt) {
+    metricSelector.disabled = true;
+
     controlPoints.push({ x: evt.offsetX, y: evt.offsetY });
-    drawVoronoiToCanvas();
+    drawDiagramToCanvas();
 }, false);
+
+function onClear() {
+    controlPoints = new Array();
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    metricSelector.disabled = false;
+}
+
+function getPNormDistance(norm, x1, x2, y1, y2) {
+    return Math.pow(Math.abs(x1 - x2), norm) + Math.pow(Math.abs(y1 - y2), norm);
+}
+
+function getNormValue() {
+    switch (metricSelector.value) {
+        case "euclidean":
+            return 2;
+        case "manhattan":
+            return 1;
+        case "3norm":
+            return 3;
+
+        default:
+            return 2;
+    }
+}
 
 function getClosestControlPointAndDistance(x, y) {
     var closest = null;
@@ -19,7 +46,8 @@ function getClosestControlPointAndDistance(x, y) {
     for (var i = 0; i < controlPoints.length; i++) {
         var controlPoint = controlPoints[i];
 
-        var currentDistance = Math.pow(controlPoint.x - x, 2) + Math.pow(controlPoint.y - y, 2);
+        var normValue = getNormValue();
+        var currentDistance = getPNormDistance(normValue, controlPoint.x, x, controlPoint.y, y);
         if (Math.abs(currentDistance) < epsilon) {
             return { controlPoint: controlPoint, distance: 0 };
         }
@@ -33,7 +61,7 @@ function getClosestControlPointAndDistance(x, y) {
     return { controlPoint: closest, distance: distance };
 }
 
-function drawVoronoiToCanvas() {
+function drawDiagramToCanvas() {
     for (var y = 0; y < size; y++) {
         for (var x = 0; x < size; x++) {
             var pointDistancePair = getClosestControlPointAndDistance(x, y);
